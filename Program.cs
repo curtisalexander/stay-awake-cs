db970d36a29abc12b5517f2a186e097344e6402b
Curtis Alexander <calex@calex.org>
2019-11-05 14:00:22 -0600

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stayawake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
