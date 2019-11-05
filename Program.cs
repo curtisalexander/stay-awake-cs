f85860622ffe58a7212d0ec3c4b109eecdd9b6fb
Curtis Alexander <calex@calex.org>
2019-11-05 14:46:20 -0600

    First tests with DragonFruit

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.CommandLine.DragonFruit;
 
 namespace stayawake
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>DragonFruit simple example program</summary>
+        /// <param name="verbose">Show verbose output</param>
+        /// <param name="flavor">Which flavor to use?</param>
+        /// <param name="count">How many smoothies?</param>
+        static int Main(bool verbose, string flavor = "strawberry", int count = 1)
         {
-            Console.WriteLine("Hello World!");
+            if (verbose)
+            {
+                Console.WriteLine("Running now in verbose mode!");
+            }
+        
+            Console.WriteLine($"Creating {count} banana {(count == 1 ? "smoothie" : "smoothies")} with {flavor}");
+            return 0;
         }
     }
 }
