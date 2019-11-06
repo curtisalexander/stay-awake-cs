e0d6a1822402657ff24b11d88a11e9b15a2b92ce
Curtis Alexander <calex@calex.org>
2019-11-05 19:30:24 -0600

    Working prototype after extensive changes.  Should have the bones of the program.  Committing before fine-tuning and continuing to validate usage.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,76 @@
 ﻿using System;
-using System.CommandLine.DragonFruit;
+using System.Runtime.InteropServices;
 
 namespace stayawake
 {
+    internal enum AwakeMode { Away, Display, System }
+
+    [FlagsAttribute]
+    internal enum ExecutionState : uint
+    {
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001
+        // Legacy flag, should not be used.
+        // ES_USER_PRESENT = 0x00000004
+    }
     class Program
     {
-        /// <summary>DragonFruit simple example program</summary>
-        /// <param name="verbose">Show verbose output</param>
-        /// <param name="flavor">Which flavor to use?</param>
-        /// <param name="count">How many smoothies?</param>
-        static int Main(bool verbose, string flavor = "strawberry", int count = 1)
+        // https://www.pinvoke.net/default.aspx/kernel32/SetThreadExecutionState.html
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+
+        private static void SetMode(AwakeMode aMode, ExecutionState eState)
         {
-            if (verbose)
+            Console.WriteLine($"Staying awake with mode: {aMode}");
+
+            try
             {
-                Console.WriteLine("Running now in verbose mode!");
+                Console.WriteLine($"Executing SetThreadExecutionState({eState} | ES_CONTINUOUS)");
+
+                SetThreadExecutionState(eState | ExecutionState.ES_CONTINUOUS);
+
+                Console.WriteLine("Press ``Enter`` to stop staying awake");
+                var _ = Console.ReadLine();
             }
-        
-            Console.WriteLine($"Creating {count} banana {(count == 1 ? "smoothie" : "smoothies")} with {flavor}");
+            catch (System.Exception e)
+            {
+                Console.WriteLine($"Unable to execute SetThreadExecutionState({eState} | ES_CONTINUOUS)");
+                Console.WriteLine($"The relevant error message is {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Stopping stay awake");
+                Console.WriteLine($"Executing SetThreadExecutionState(ES_CONTINUOUS)");
+
+                SetThreadExecutionState(ExecutionState.ES_CONTINUOUS);
+            }
+        }
+
+        // powercfg -requests
+        /// <summary>keep 
+        /// <para>stayawake keeps the computer awake</para>
+        /// </summary>
+        /// <param name="awakeMode">Which mode to use?</param>
+        static int Main(AwakeMode awakeMode)
+        {
+            switch(awakeMode)
+            {
+                case AwakeMode.Away:
+                    SetMode(awakeMode, ExecutionState.ES_AWAYMODE_REQUIRED);
+                    break;
+                case AwakeMode.Display:
+                    SetMode(awakeMode, ExecutionState.ES_DISPLAY_REQUIRED);
+                    break;
+                case AwakeMode.System:
+                    SetMode(awakeMode, ExecutionState.ES_SYSTEM_REQUIRED);
+                    break;
+                default:
+                    Console.WriteLine("Awake mode has not been set.");
+                    break;
+            }
+
             return 0;
         }
     }
